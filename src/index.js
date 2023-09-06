38c328483cc5e61fbace74f761851610771f2a61
Siddhant Kumar <siddhuyadav662@gmail.com>
2023-09-06 14:41:02 +0530

    Completed

--- src/index.js
+++ src/index.js
@@ -1,31 +1,33 @@
-const express=require('express');
-const app =express();
+const express = require('express');
+const app = express();
+const bcrypt = require('bcrypt');
+const path = require('path');
 
-const path=require('path');
+const collection = require("./mongodb")
 
-const collection=require("./mongodb")
-
-const templatepath=path.join(__dirname,'../templates');
+const templatepath = path.join(__dirname, '../templates');
 app.use(express.json());
-app.set("view engine","ejs");
-app.set('views',templatepath);
+app.set("view engine", "ejs");
+app.set('views', templatepath);
 
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
 
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.render('login')
 });
-app.get('/signup',(req,res)=>{
+app.get('/signup', (req, res) => {
     res.render('signup')
 });
 
-app.post('/signup',async (req,res)=>{
+app.post('/signup', async (req, res) => {
     // definition of signup data
-    const data={
-        name:req.body.name,
-        password:req.body.password
+    var { name, password } = req.body;
+    let hashedPassword = await bcrypt.hash(password, 10);
+    const data = {
+        name: name,
+        password: hashedPassword
     }
     // send to mongoose
     await collection.insertMany([data]);
@@ -33,25 +35,22 @@ app.post('/signup',async (req,res)=>{
 })
 
 
-app.post('/login',async (req,res)=>{
-    try{
-        const check =await collection.findOne({name:req.body.name});
+app.post('/login', async (req, res) => {
+    try {
+        const check = await collection.findOne({ name: req.body.name });
 
-        if(check.password===req.body.password)
-        {
+        if (check.password === req.body.password) {
             res.render("home");
         }
-        else{
+        else {
             res.send("wrofn pass");
         }
     }
-    catch{
+    catch {
         res.send("wring deltial");
     }
 })
 
-
-
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("connected");
 });
--- src/mongodb.js
+++ src/mongodb.js
@@ -1,27 +1,25 @@
-const mongoose=require('mongoose');
-const {Schema} = mongoose;
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
 mongoose.connect("mongodb://0.0.0.0:27017/loginn")
-.then(()=>{
-    console.log("DataBase is Connected");
-})
-.catch((e)=>{
-    console.log("Failed to Connect");
-});
+    .then(() => {
+        console.log("DataBase is Connected");
+    })
+    .catch((e) => {
+        console.log("Failed to Connect");
+    });
 
-const logINSchema= new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
+const logINSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+        type: String,
+        required: true
     }
 });
 
-const collection=new mongoose.model('login1',logINSchema);
-
-
-module.exports=collection;
+const collection = new mongoose.model('login1', logINSchema);
+module.exports = collection;
 
